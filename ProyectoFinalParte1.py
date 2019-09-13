883bb29a91e5daea1b46ff00952db730f53ea8dc
Xavier Vargas <54075664+Xavierification@users.noreply.github.com>
2019-09-12 23:21:05 -0500

    Add files via upload
    Parte inicial del Script con varios Switches creados

---
+++ ProyectoFinalParte1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='100.0.0.0/24')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+
+
+
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+
+
+
+    info( '*** Post configure switches and hosts\n')
+
+
+    net.stop()
+    exit()
+    	
+	
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
+
+
