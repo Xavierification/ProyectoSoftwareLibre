d8701d490ec0ca1be52b111a3466abc92f5f7e8e
Xavier <hitlermario8@gmail.com>
2019-09-12 23:57:26 -0500

    Segunda versión del Proyecto agregando la ip correcta y los Switches completos

---
+++ ProyectoFinalParte2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+from os import sys
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='191.4.199.0/24')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+    s7 = net.addSwitch('s7', cls=OVSKernelSwitch)
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s8 = net.addSwitch('s8', cls=OVSKernelSwitch)
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
+
+
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+
+
+
+    info( '*** Post configure switches and hosts\n')
+
+
+    net.stop()
+    exit()
+    	
+	
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
+
+
